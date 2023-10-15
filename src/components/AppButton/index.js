037083b412aefa7b7491f569e6087a30c6162943
Mucahit <mucahit.tunel42@gmail.com>
2023-10-15 12:58:17 +0300

    Dosya düzeni oluşturuldu

---
+++ src/components/AppButton/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const AppButton = () => {
+  return (
+    <TouchableOpacity>
+      
+    </TouchableOpacity>
+  )
+}
+
+export default AppButton
+
+const styles = StyleSheet.create({})
